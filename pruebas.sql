780f254f3a414972b2eac380b76aa82fd13e82a4
karixcm <147661473+karixcm@users.noreply.github.com>
2023-10-15 17:54:30 -0600

    Update pruebas.sql

--- pruebas.sql
+++ pruebas.sql
@@ -1,17 +1,20 @@
 USE ComedorBD
 GO
 
-EXEC PROC_statsPaises;
-GO
+--EXEC PROC_statsPaises;
+--GO
 
-EXEC PROC_statsCondicion;
-GO
+--EXEC PROC_statsCondicion;
+--GO
 
-EXEC PROC_contarPersonasSinHogarMexicanas;
-GO
+--EXEC PROC_contarPersonasSinHogarMexicanas;
+--GO
 
-EXEC PROC_ordenMejoresComedores '10','2022';
-GO
+--EXEC PROC_ordenMejoresComedores '10','2022';
+--GO
 
-EXEC PROC_buscarFamiliares '1001';
+--EXEC PROC_buscarFamiliares '1001';
+--GO
+
+EXEC PROC_listaComedores;
 GO
